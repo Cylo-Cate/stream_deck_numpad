288509b986444e55c9dc93d2d6304726273c2e8e
Cylo-cate <6364.143@gmail.com>
2026-06-17 19:18:06 -0300

    Adding Different Functions for the keys

--- config.py
+++ config.py
@@ -1,15 +1,43 @@
 import os
+import webbrowser
+import keyboard
 
 def open_file(file):
     os.startfile(file)
+def open_url(site):
+    webbrowser.open(site)
+def pause_play(_):
+    keyboard.send("play/pause media")
+def shortcuts(binds):
+    keyboard.send(binds)
+
 
 
 
 keys = [
     { #1
-        "name": "Action 1",
+        "name": "Lumi",
         "action": open_file,
         "shortcut": r"C:\Users\6364.143\Desktop\Lumi.lnk",
-        "icon": "obs.png"
+    },
+    { #2
+        "name": "Gmail",
+        "action": open_url,
+        "shortcut": "https://mail.google.com/mail/u/0/?ogbl#inbox",
+    },
+    { #3
+        "name": "Play/Pause",
+        "action": pause_play,
+        "shortcut": "",
+    },
+     { #4
+        "name": "Manager",
+        "action": shortcuts,
+        "shortcut": "ctrl+shift+esc",
+    },
+    { #4
+        "name": "Mute Discord",
+        "action": shortcuts,
+        "shortcut": "ctrl+shift+m",
     },
 ]
--- listener.py
+++ listener.py
@@ -1,6 +1,5 @@
 import os
 import keyboard
-import config
 from config import keys
 
 solo = True
@@ -14,6 +13,11 @@ def switch(event):
     solo = not solo
 
 keyboard.on_press_key("-", switch, suppress=True)
-keyboard.on_press_key("1", lambda e: keys[0]["action"](keys[0]["shortcut"]))
+keyboard.on_press_key("1", lambda e: keys[0]["action"](keys[0]["shortcut"]), suppress=True)
+keyboard.on_press_key("2", lambda e: keys[1]["action"](keys[1]["shortcut"]), suppress=True)
+keyboard.on_press_key("3", lambda e: keys[2]["action"](keys[2]["shortcut"]), suppress=True)
+keyboard.on_press_key("4", lambda e: keys[3]["action"](keys[3]["shortcut"]), suppress=True)
+keyboard.on_press_key("5", lambda e: keys[4]["action"](keys[4]["shortcut"]), suppress=True)
 
-keyboard.wait()
+
+keyboard.wait('esc')
