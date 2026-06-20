da2edbd36b36012ae0fa9b8a2a9a3e7efdf6e4d9
Cylocate <fegustavorc@gmail.com>
2026-06-20 19:50:25 -0300

    Configuring Numpad binds

--- config.py
+++ config.py
@@ -10,34 +10,114 @@ def pause_play(_):
     keyboard.send("play/pause media")
 def shortcuts(binds):
     keyboard.send(binds)
+    
+Numpad_Map = {
+    69: 0,   # NumLock
+    53: 1,   # /
+    55: 2,   # *
+    74: 3,   # -
+    71: 4,   # 7
+    72: 5,   # 8
+    73: 6,   # 9
+    78: 7,   # +
+    75: 8,   # 4
+    76: 9,   # 5
+    77: 10,  # 6
+    79: 11,  # 1
+    80: 12,  # 2
+    81: 13,  # 3
+    82: 14,  # 0
+    126: 15, # .
+    28: 16,  # Enter
+}
 
 
 
 
 keys = [
-    { #1
+    { #NumLock
         "name": "Lumi",
         "action": open_file,
         "shortcut": r"C:\Users\6364.143\Desktop\Lumi.lnk",
     },
-    { #2
+    { #/
         "name": "Gmail",
         "action": open_url,
         "shortcut": "https://mail.google.com/mail/u/0/?ogbl#inbox",
     },
-    { #3
+    { #*
         "name": "Play/Pause",
         "action": pause_play,
         "shortcut": "",
     },
-     { #4
+    { #-
         "name": "Manager",
         "action": shortcuts,
         "shortcut": "ctrl+shift+esc",
     },
-    { #4
+    { #7
         "name": "Mute Discord",
         "action": shortcuts,
         "shortcut": "ctrl+shift+m",
+    },
+    { #8
+        "name": "",
+        "action": "",
+        "shortcut": "",
+    },
+    { #9
+        "name": "",
+        "action": "",
+        "shortcut": "",
+    },
+    { #+
+        "name": "",
+        "action": "",
+        "shortcut": "",
+    },
+    { #4
+        "name": "",
+        "action": "",
+        "shortcut": "",
+    },
+    { #5
+        "name": "",
+        "action": "",
+        "shortcut": "",
+    },
+    { #6
+        "name": "",
+        "action": "",
+        "shortcut": "",
+    },
+    { #1
+        "name": "A",
+        "action": open_url,
+        "shortcut": "www.youtube.com",
+    },
+    { #2
+        "name": "",
+        "action": "",
+        "shortcut": "",
+    },
+    { #3
+        "name": "",
+        "action": "",
+        "shortcut": "",
+    },
+    { #Enter
+        "name": "",
+        "action": "",
+        "shortcut": "",
+    },
+    { #0
+        "name": "",
+        "action": "",
+        "shortcut": "",
+    },
+    { #.
+        "name": "",
+        "action": "",
+        "shortcut": "",
     },
 ]
--- listener.py
+++ listener.py
@@ -1,23 +1,21 @@
 import os
 import keyboard
-from config import keys
+from config import keys, Numpad_Map
 
 solo = True
 
-def ativar_lumi(name):
-    if solo:
-        os.startfile(r"C:\Users\6364.143\Desktop\Lumi.lnk")
+def on_press_key(event):
+    if event.event_type != "down":
+        return
+    
+    index = Numpad_Map.get(event.scan_code)
+    
+    if index != None:
+        keys[index]["action"](keys[index]["shortcut"])
 
-def switch(event):
-    global solo
-    solo = not solo
+# def switch(event):
+#     global solo
+#     solo = not solo
 
-keyboard.on_press_key("-", switch, suppress=True)
-keyboard.on_press_key("1", lambda e: keys[0]["action"](keys[0]["shortcut"]), suppress=True)
-keyboard.on_press_key("2", lambda e: keys[1]["action"](keys[1]["shortcut"]), suppress=True)
-keyboard.on_press_key("3", lambda e: keys[2]["action"](keys[2]["shortcut"]), suppress=True)
-keyboard.on_press_key("4", lambda e: keys[3]["action"](keys[3]["shortcut"]), suppress=True)
-keyboard.on_press_key("5", lambda e: keys[4]["action"](keys[4]["shortcut"]), suppress=True)
-
-
+keyboard.hook(on_press_key)
 keyboard.wait('esc')
